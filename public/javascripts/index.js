3ed5a9f2699c2252b432974a9ad2887a94f88a26
Noel Ramirez <noel.rmrz@gmail.com>
2015-08-22 15:28:14 -0700

    first commit

---
+++ public/javascripts/index.js
@@ -0,0 +1,317 @@
+var stage;
+var triangle;
+var sphereContainer = new createjs.Container();
+var bulletContainer = new createjs.Container();
+var enemyContainer = new createjs.Container();
+var particleContainer = new createjs.Container();
+var specialContainer = new createjs.Container();
+var enemies = [];
+var spheres = [];
+var particles = [];
+var oldX;
+var oldY;
+var degToRad = -Math.PI / 180;
+var score = 0;
+var nIntervId;
+var sonic = new createjs.Shape();
+
+$(document).ready(function() {
+  var context = $('canvas')[0].getContext('2d');
+  context.canvas.width = 500;
+  context.canvas.height = 500;
+
+  stage = new createjs.Stage('canvas');
+  stage.mouseEventsEnabled = true;
+
+  createjs.Ticker.setFPS(60);
+  createjs.Ticker.on('tick', tick);
+  createjs.Ticker.addEventListener('tick', stage);
+ 
+  triangle = new createjs.Shape();
+  triangle.graphics.beginFill('DeepSkyBlue');
+  triangle.graphics.moveTo(0, 0).lineTo(10, 15).lineTo(0, 30).lineTo(30, 15).lineTo(0, 0);
+  triangle.regX = 15;
+  triangle.regY = 15;
+  triangle.x = triangle.y = 100;
+
+  bulletContainer.x = 0;
+  bulletContainer.y = 0;
+
+  stage.addChild(sphereContainer);
+  stage.addChild(triangle);
+  stage.addChild(bulletContainer);
+  stage.addChild(specialContainer);
+  stage.addChild(enemyContainer);
+  stage.addChild(particleContainer);
+  
+  stage.on("stagemousemove", function(evt) {
+    if (oldX) {
+      triangle.x = evt.stageX;
+      triangle.y = evt.stageY;
+    }
+
+    oldX = evt.stageX;
+    oldY = evt.stageY;
+  })
+  
+  nIntervId = setInterval(addSphere, 1000 * getRandomIntInclusive(0.1, 1));
+
+  stage.on("stagemousedown", shoot)
+  stage.on("stagemouseup", addEnemy)
+});
+
+function tick(event) {
+  /**Move projectiles**/
+  bulletContainer.children.forEach(moveProjectiles);
+
+  //if (bulletContainer.children.length > 0 ) {
+    for (var x = 0; x < bulletContainer.children.length; x++) {
+      for (var y = 0; y < enemyContainer.children.length; y++) {
+        var xDistance = bulletContainer.children[x].x - enemyContainer.children[y].x;
+        var yDistance = bulletContainer.children[x].y - enemyContainer.children[y].y;
+        var distance = pythagorus(xDistance, yDistance);
+
+        if (distance < 5 + 25) {
+          destroyEnemy(x, y);
+        }
+      }
+    }
+  //}
+  
+  if (specialContainer.children.length > 0) {
+    specialContainer.children[0].graphics.clear();
+    specialContainer.children[0].radius += 5;
+    increaseSuperSonic(specialContainer.children[0], specialContainer.children[0].radius);
+  }
+
+  handleComplete();
+  particleContainer.children.forEach(moveParticles);
+  enemyContainer.children.forEach(moveEnemies);
+  sphereContainer.children.forEach(moveBackground);
+}
+
+/*** Enemy Functions ***/
+function destroyEnemy(bulletIndex, enemyIndex) {
+  createjs.Tween.get(enemyContainer.children[enemyIndex])
+    .to({alpha:0, visible:false}, 100);
+  createjs.Tween.get(bulletContainer.children[bulletIndex])
+    .to({alpha:0, visible:false}, 100);
+  
+  incrementScore(100);
+  addParticles(enemyContainer.children[enemyIndex].x, enemyContainer.children[enemyIndex].y, getRandomIntInclusive(7, 17));
+
+  setTimeout(cleanContainers(bulletIndex, enemyIndex), 100);
+}
+
+function moveEnemies(element, index, array) {
+  enemyContainer.children[index].x += (enemies[index].run * enemies[index].xDirection);
+  enemyContainer.children[index].y += (enemies[index].rise * enemies[index].yDirection);
+}
+
+function createEnemy(enemy) {
+  this.shape = enemy;
+  this.yDirection = 1;
+  this.xDirection = 1;
+  this.rise = Math.floor(Math.random() * 11);
+  this.run = Math.floor(Math.random() * 11);
+}
+
+function addEnemy() {
+  var enemy = new createjs.Shape();
+  enemy.graphics.beginFill('black').drawCircle(0, 0, 25);
+  enemy.x = Math.random() * stage.canvas.width;
+  enemy.y = Math.random() * stage.canvas.height;
+  enemy.alpha = 0;
+
+  enemies.push(new createEnemy(enemy));
+
+  enemyContainer.addChild(enemies[enemies.length - 1].shape);
+  createjs.Tween.get(enemies[enemies.length - 1].shape)
+    .to({alpha:1, visible:true}, 500);
+    //.call(handleComplete);
+  /*for (var i = 0; i < 20; i++) {
+    var circle = new createjs.Shape();
+    circle.graphics.setStrokeStyle(15);
+    circle.graphics.beginStroke('#113355');
+    circle.graphics.drawCircle(0, 0, (i + 1) * 4);
+    circle.alpha = 1 - i * 0.02;
+    circle.x = 100;
+    circle.y = 100;
+    circle.compositeOperation = 'lighter';
+
+    enemyContainer.addChild(circle);
+  }*/
+}
+
+/*** Projectile Funcdtions ***/
+function shoot() {
+  var shot = new createjs.Shape();
+  shot.graphics.beginFill('black').drawCircle(0, 0, 5);
+  shot.regX = 0;
+  shot.regY = 0;
+  shot.x = triangle.x;
+  shot.y = triangle.y;
+  shot.rotation = triangle.rotation;
+
+  bulletContainer.addChild(shot);
+}
+
+function moveProjectiles(element, index, array) {
+  var rise = Math.sin(array[index].rotation * degToRad) * 30;
+  var run = Math.cos(array[index].rotation * degToRad) * 30;
+
+  array[index].x += run;
+  array[index].y -= rise;
+
+  /**Remove offstage bulletContainer**/
+  if (array[index].x < 0 || array[index].x > stage.canvas.width)
+    bulletContainer.removeChildAt(index);
+  else if (array[index].y < 0 || array[index].y > stage.canvas.height)
+    bulletContainer.removeChildAt(index);
+}
+
+function pythagorus(a, b) {
+  return Math.sqrt(Math.pow(a, 2) +  Math.pow(b, 2));
+}
+
+function incrementScore(amount) {
+  score += amount;
+}
+
+function cleanContainers(bulletIndex, enemyIndex) {
+  console.log(enemyContainer.removeChildAt(enemyIndex));
+  console.log(bulletContainer.removeChildAt(bulletIndex));
+}
+
+function moveBackground(element, index, array) {
+  sphereContainer.children[index].y -= spheres[index].rise;
+  
+  createjs.Tween.get(spheres[index].shape)
+    .to({alpha:1, visible:true}, 500);  
+  
+  if (array[index].y < -15)
+    sphereContainer.removeChildAt(index);
+}
+
+function handleComplete() {
+  for (var x = 0; x < enemyContainer.children.length; x++) {
+    //**Check width boundaries**//
+    if (enemyContainer.children[x].y < 25 || enemyContainer.children[x].y + 25 > stage.canvas.height) {
+      enemies[x].yDirection = -(enemies[x].yDirection);
+    }
+    
+    //**Check height boundaries**//
+    if (enemyContainer.children[x].x < 25 || enemyContainer.children[x].x + 25 > stage.canvas.width) {
+      enemies[x].xDirection = -(enemies[x].xDirection);
+    }
+  }
+}
+
+function addSphere() {
+  var sphere = new createjs.Shape();
+  sphere.graphics.beginFill(createjs.Graphics.getHSL(Math.random()*360,100,50)).drawCircle(0,0,30);
+  sphere.x = Math.random() * stage.canvas.width;
+  sphere.y = getRandomIntInclusive(stage.canvas.height, stage.canvas.height + 15);
+  sphere.alpha = 0;  
+  
+  spheres.push(new createSphere(sphere));
+
+  sphereContainer.addChild(spheres[spheres.length - 1].shape);
+  createjs.Tween.get(spheres[spheres.length - 1].shape)
+    .to({alpha:0.1, visible:true}, 500);  
+}
+
+function createSphere(sphere) {
+  this.shape = sphere;
+  this.rise = getRandomIntInclusive(1, 5); 
+}
+
+function getRandomIntInclusive(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function particleEmitter() {
+  
+}
+
+function moveParticles(element, index, array) {
+  particleContainer.children[index].x += (particles[index].run * particles[index].xDirection);
+  particleContainer.children[index].y += (particles[index].rise * particles[index].yDirection);
+}
+
+function addParticles(x, y, amount) {
+  while (amount > 0) {
+  var particle = new createjs.Shape();
+  
+  if (getRandomIntInclusive(0, 1) > 0) {
+    particle.graphics.setStrokeStyle(1).beginStroke(createjs.Graphics.getHSL(Math.random()*360,100,50)).drawCircle(0, 0, getRandomIntInclusive(5, 25));
+  }
+  else {
+    particle.graphics.beginFill(createjs.Graphics.getHSL(Math.random()*360,100,50)).drawCircle(0, 0, getRandomIntInclusive(5, 25));
+    var blurFilter = new createjs.BlurFilter(5, 5, 1);
+    particle.filters = [blurFilter];
+    var bounds = blurFilter.getBounds();
+      
+    particle.cache(-50+bounds.x, -50+bounds.y, 100+bounds.width, 100+bounds.height);
+  }
+  
+  particle.x = x;
+  particle.y = y;
+  particle.alpha = 1;
+
+  particles.push(new createParticle(particle));
+
+  particleContainer.addChild(particles[particles.length - 1].shape);
+  amount--;
+  }
+  
+  for (x = 0; x < particles.length; x++) {
+    createjs.Tween.get(particles[x].shape)
+      .to({alpha:0, visible:false}, 400);
+  }
+}
+
+function createParticle(particle) {
+  this.shape = particle;
+  
+  if (getRandomIntInclusive(0, 1) > 0) {
+    this.yDirection = 1;
+  }
+  else
+    this.yDirection = -1;
+  
+  if (getRandomIntInclusive(0, 1) > 0) {
+    this.xDirection = 1;
+  }
+  else
+    this.xDirection = -1;
+  
+  this.rise = Math.floor(Math.random() * 11);
+  this.run = Math.floor(Math.random() * 11);
+}
+
+function superSonic() {
+  sonic.radius = 30;
+  sonic.graphics.setStrokeStyle(10).beginStroke(createjs.Graphics.getHSL(Math.random()*360,100,50)).drawCircle(0, 0, sonic.radius);
+  
+  sonic.x = triangle.x;
+  sonic.y = triangle.y;
+  
+  specialContainer.addChild(sonic);
+}
+
+function increaseSuperSonic(aShape, radius) {
+  aShape.graphics.setStrokeStyle(10).beginStroke(createjs.Graphics.getHSL(Math.random()*360,100,50)).drawCircle(0, 0, radius);
+}
+
+$(document).keydown(function(event) {
+  if (event.which == 37) {
+    triangle.rotation--;
+  }
+  else if (event.which == 39) {
+    triangle.rotation++;
+  }
+  else if (event.which == 32) {
+    superSonic();
+  }
+});
